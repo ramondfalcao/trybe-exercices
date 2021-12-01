0aba4d7a051b4bff4554a28cb412ee473d355920
Ramond Falcão <ramondfalcao@gmail.com>
2021-12-01 18:59:15 -0300

    Exercicios objetos e for/in

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/scripts.js
@@ -1,18 +1,48 @@
-let conta = {
-    agencia: '0000',
-    banco: {
-      cod: '012',
-      id: 4,
-      nome: 'TrybeBank',
+// let info1= {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// }; let info2 = {
+//   personagem: 'Tio Patinhas',
+//   origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+//   nota: 'O último MacPatinhas',
+// };
+
+
+// // console.log("Bem vinda, " + info.personagem)
+// info1.recorrente = "sim"
+// info2.recorrente = "sim"
+
+// for ( let index in info2){
+//   console.log(info1.personagem + " e " + info2.personagem)
+//   console.log(info1.origem + info2.origem)
+//   console.log(info1.nota + " e " + info2.nota)
+//   if (info1.recorrente === "sim" && info2.recorrente === "sim"){
+//     console.log("Ambos recorrentes")
+//   } break
+// }
+
+//////////////////////////////////////
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
     },
-  };
-  
-  let infoDoBanco = 'banco';
-  console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
-  console.log(conta[infoDoBanco]['nome']); // TrybeBank
-  
-  console.log(conta.agencia); // 0000
-  console.log(conta['agencia']); // 0000
-  
-  console.log(conta.banco.cod); // 012
-  console.log(conta['banco']['id']); // 4
+  ],
+};
+
+console.log("O livro favorito de " + leitor.nome + " se chama " + leitor.livrosFavoritos[0].titulo+ ".")
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editor: 'Rocco'
+})
+
+console.log(leitor.livrosFavoritos[1])
