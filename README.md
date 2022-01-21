247d0615e92fef998c0db76f7dfb369a4343041a
Ramond Falcão <ramondfalcao@gmail.com>
2022-01-21 14:54:17 -0300

    atualizando readme

--- README.md
+++ README.md
@@ -63,8 +63,8 @@ Contém todas as atividades desenvolvidas por __[mim](https://www.linkedin.com/i
 ##### Bloco 8: Higher Order Functions do JavaScript ES6
 - [x] 8.1 - JavaScript ES6 - Introdução a Higher Order Functions
 - [x] 8.2 - JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort
-- [] 8.3 - JavaScript ES6 - Higher Order Functions - map e filter
-- [] 8.4 - JavaScript ES6 - Higher Order Functions - reduce
+- [x] 8.3 - JavaScript ES6 - Higher Order Functions - map e filter
+- [x] 8.4 - JavaScript ES6 - Higher Order Functions - reduce
 - [] 8.5 - JavaScript ES6 - spread operator, parâmetro rest, destructuring e mais
 - [] 8.6 - Projeto - Zoo functions
 
--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-map-e-filter/script.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-map-e-filter/script.js
@@ -1,16 +1,114 @@
-const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
 
-const prices = [2.99, 3.99, 1.5, 2];
+// Adicione o código do exercício aqui:
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
 
-const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
-  { [product]: listPrices[index] }
-));
+const formatedBookNames = books.map((book) => `${book.name}-${book.genre}-${book.author.name}`);
 
-const listProducts = updateProducts(products, prices);
-console.log(listProducts);
-// => [
-//   { Arroz: 2.99 },
-//   { Feijao: 3.99 },
-//   { Alface: 1.5 },
-//   { Tomate: 2 },
-// ]
+// console.log(formatedBookNames)
+
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+function nameAndAge() {
+  return books
+    .map((book) => (
+      {
+        author: book.author.name,
+        age: book.releaseYear - book.author.birthYear,
+      }
+    ))
+    .sort((obj1, obj2) => obj1.age - obj2.age);
+}
