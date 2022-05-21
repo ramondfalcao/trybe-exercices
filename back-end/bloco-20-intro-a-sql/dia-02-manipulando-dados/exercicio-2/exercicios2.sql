1cdc8c342714a0ebe11f37ee0d61d5760ea47741
Ramond Falcão <ramondfalcao@gmail.com>
2022-05-21 10:42:35 -0300

    Exercicio 2 Concluido

---
+++ back-end/bloco-20-intro-a-sql/dia-02-manipulando-dados/exercicio-2/exercicios2.sql
@@ -0,0 +1,20 @@
+
+-- 1- Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT.
+
+SELECT Name AS RBT FROM Pieces WHERE Code = 2;
+
+-- 2- Escreve uma query para exibir todas as informações das cinco peças com os maiores preços.
+
+SELECT Piece, Provider, Price FROM Provides ORDER BY Price DESC LIMIT 5;
+
+-- 3- Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.
+
+SELECT Provider, Price FROM Provides ORDER BY Price DESC LIMIT 4 OFFSET 3;
+
+-- 4- Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL. Ordene o resultado pelos preços das peças de forma decrescente.
+
+SELECT * FROM Provides WHERE Provider = 'HAL' ORDER BY Price DESC;
+
+-- 5- Escreva uma query para exibir por quantas empresas a peça 1 é provida.
+
+SELECT COUNT(Provider) FROM Provides WHERE Piece = '1';
