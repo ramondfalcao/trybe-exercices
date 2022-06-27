118317c1bc7c6ab52a9a8edf00d6930f70cb8ff1
Ramond Falcão <ramondfalcao@gmail.com>
2022-06-27 19:35:12 -0300

    Arquivo CepModel para requisição do cep no banco de dados

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-camada-de-controller-e-service/cep-lookup/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-camada-de-controller-e-service/cep-lookup/index.js
@@ -7,6 +7,8 @@ app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong!' });
 });
 
+app.get('/cep/:cep')
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}`); });
