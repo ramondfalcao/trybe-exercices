18242785d586a7273b11e1b04a722f49cb9c7238
Ramond Falcão <ramondfalcao@gmail.com>
2022-07-12 14:59:20 -0300

    feat: Exe-7 Controller getByICD

--- back-end/bloco-24-orm-e-autenticação/dia-01-intro-a-orm/app-with-sequelize/controllers/BooksController.js
+++ back-end/bloco-24-orm-e-autenticação/dia-01-intro-a-orm/app-with-sequelize/controllers/BooksController.js
@@ -5,6 +5,15 @@ const getAll = async (req, res) => {
   res.status(200).json(books);
 };
 
+const getById = async (req, res) => {
+  const book = await BookService.getById(req.params.id);
+
+  if (!book) return res.status(404).json({ message: 'Book not found' });
+
+  res.status(200).json(book);
+};
+
 module.exports = {
   getAll,
+  getById,
 };
