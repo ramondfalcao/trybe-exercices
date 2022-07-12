f879ed15ce39f9aef793552fd70438269209e433
Ramond Falcão <ramondfalcao@gmail.com>
2022-07-12 15:03:13 -0300

    feat: Rota POST para criação de livros

--- back-end/bloco-24-orm-e-autenticação/dia-01-intro-a-orm/app-with-sequelize/index.js
+++ back-end/bloco-24-orm-e-autenticação/dia-01-intro-a-orm/app-with-sequelize/index.js
@@ -7,5 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/books', BooksController.getAll);
+app.get('/books/:id', BooksController.getById);
+app.post('/books', BooksController.create);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
